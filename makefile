c0166ac8db7348725fe4c86ef118a8c27c9f057e
ToooooooT <linyugung@gmail.com>
2022-06-18 21:07:21 +0800

    Seperate read and write screen
    Use two terminal to seperate read and write screen.

--- makefile
+++
@@ -1,8 +0,0 @@
-all: socket_server socket_client
-
-socket_server: socket_server.c
-	gcc socket_server.c -lpthread -o socket_server
-socket_client: socket_client.c
-	gcc socket_client.c -lpthread -o socket_client
-clean:
-	rm -rf socket_server socket_client
--- socket_client.c
+++ socket_client.c
@@ -1,141 +1,17 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h> 
-#include <pthread.h>
-#include <semaphore.h>
 
-void *thread_read_chat();
-void *thread_send_text();
+int main (int argc, char *argv[]) {
+    char command[512];
+    sprintf(command, "gnome-terminal --title=\"%s's read\" --geometry=70x3+10+620 -- ./socket_client_read 127.0.0.1 %s", argv[1], argv[1]);
+    FILE *fp1 = popen(command, "w");
 
-enum {SECS_TO_SLEEP = 0, NSEC_TO_SLEEP = 125};
+    sprintf(command, "gnome-terminal --title=\"%s's chatroom\" --geometry=70x30+10 -- ./socket_client_chatroom 127.0.0.1 %s", argv[1], argv[1]);
+    FILE *fp2 = popen(command, "w");
 
-bool toQuit = false;
+    pclose(fp1);
+    pclose(fp2);
 
-int main(int argc, char *argv[])
-{
-    int sockfd = 0;
-    char recvBuff[1024], message[1024];
-    struct sockaddr_in serv_addr;
-
-    if(argc != 3) {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
-        return 1;
-    }
-    
-    memset(recvBuff, 0 ,sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    } 
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000); 
-
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0) {   
-        printf("\n inet_pton error occured\n");
-        return 1;
-    }
-
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\n Error : Connect Failed \n");
-        return 1;
-    }
-
-    memset(message, 0, 1024);
-    sprintf(message, "%s:read", argv[2]);
-    write(sockfd, message, strlen(message));
-
-    pthread_t tid_send_text;
-    if (pthread_create(&tid_send_text, NULL, thread_send_text, &sockfd) != 0) {
-        printf("Error : pthread_create\n");
-    }
-
-    sleep(1);
-
-    pthread_t tid_read_chat;
-    if (pthread_create(&tid_read_chat, NULL, thread_read_chat, argv) != 0) {
-        printf("Error : pthread_create\n");
-    }
-
-    pthread_exit(NULL);
     return 0;
 }
 
-void *thread_read_chat(void *vargp)
-{
-    int sockfd = 0;
-    char recvBuff[1024], message[1024];
-    char **argv = vargp;
-    struct sockaddr_in serv_addr;
-
-    pthread_detach(pthread_self());
-    memset(recvBuff, 0 ,sizeof(recvBuff));
-
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Error : Could not create socket \n");
-        return NULL;
-    }
-    memset(&serv_addr, '0', sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
-
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0) {   
-        printf("\n inet_pton error occured\n");
-        return NULL;
-    }
-
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\n Error : Connect Failed \n");
-        return NULL;
-    }
-
-    memset(message, 0, 1024);
-    sprintf(message, "%s:write", argv[2]);
-    write(sockfd, message, strlen(argv[2]));
-
-    while (!toQuit) {
-        memset(recvBuff, 0, sizeof(recvBuff));
-        read(sockfd, recvBuff, sizeof(recvBuff));
-        write(sockfd, "1", 2);
-        printf("%s\n", recvBuff);
-    }
-    write(sockfd, "", 2);
-    close(sockfd);
-    return NULL;
-}
-
-void *thread_send_text(void *vargp)
-{
-    int sockfd = *(int *)vargp;
-    char message[1024];
-
-    struct timespec remaining, request = {SECS_TO_SLEEP, NSEC_TO_SLEEP};
-
-    pthread_detach(pthread_self());
-    while (!toQuit) {
-        memset(message, 0, sizeof(message)); 
-        fgets(message, 1024, stdin);
-        message[strlen(message) - 1] = 0;
-        if (message[0] == '/') {
-            if (!strcmp(message, "/quit")) {
-                toQuit = true;
-                message[0] = 0;
-            }
-        }
-        write(sockfd, message, strlen(message) + 1);
-        
-        nanosleep(&request, &remaining);
-    }
-    close(sockfd);
-    return NULL;
-}
